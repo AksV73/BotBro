69bd53394299d13b5a43aeff0942dd54aba68c01
Akshaya Vijay <akshayavijay109@gmail.com>
2020-08-18 00:05:18 +0530

    add

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 chat.py
+web: python chat.py
--- chat.py
+++ chat.py
@@ -51,10 +51,11 @@ def main():
     dp.add_handler(CommandHandler('birdie',birdie))
     dp.add_handler(CommandHandler('meow',meow))
     
+   # updater.start_polling()
     updater.start_webhook(listen="0.0.0.0",
                            port=int(PORT) ,
                            url_path='1226224829:AAGK-3rL1J-O-kYLZkRjzkleZC-aWMZujEc')
-    updater.bot.setWebhook('https://botbroanimals.herokuapp.com/'+'1226224829:AAGK-3rL1J-O-kYLZkRjzkleZC-aWMZujEc' )
+    updater.bot.set_webhook('https://botbroanimals.herokuapp.com/'+'1226224829:AAGK-3rL1J-O-kYLZkRjzkleZC-aWMZujEc' )
     updater.idle()
 
 if __name__ == '__main__':
